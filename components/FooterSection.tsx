a5682b07de06d262f3d8b5c96a7f64023d55561b
Koshambi Bardhan <bardhankoshambi2005@gmail.com>
2025-01-11 17:18:59 +0530

    feat: add footer component

---
+++ components/FooterSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterSection = () => {
+  return (
+    <footer className="text-center py-4 w-full">
+      <p>Powered by IEEE Computer Society</p>
+    </footer>
+  );
+};
+
+export default FooterSection;
