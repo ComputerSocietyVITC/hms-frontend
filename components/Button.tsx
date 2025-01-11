0daaa2c6538fc5bc961b681e07d775a3d9b407e6
Bhargavi <dbhargavi.deshmukh@gmail.com>
2025-01-11 23:55:08 +0530

    feat: add button

---
+++ components/Button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ButtonProps {
+  customBackgroundColor: string;
+  customTextColor: string;
+  buttonText: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  customBackgroundColor = "#1E1E1E",
+  customTextColor = "#F5F5F5",
+  buttonText,
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: customBackgroundColor,
+        color: customTextColor,
+      }}
+      className="rounded-lg py-2 px-4 focus:outline-none transition duration-300 ease-in-out hover:bg-gray-800"
+    >
+      {buttonText}
+    </button>
+  );
+};
+
+export default Button;
