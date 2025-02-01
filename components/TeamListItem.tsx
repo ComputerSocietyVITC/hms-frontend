51d1dbc82263b7f56e2ed87fcb04525572171307
Aayush Shukla <oreus.aks@gmail.com>
2025-02-01 23:39:01 +0530

    fix: update styles and use Image tag

--- components/TeamListItem.tsx
+++ components/TeamListItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface TeamListItemProps {
   teamId: string;
@@ -14,23 +15,25 @@ const TeamListItem: React.FC<TeamListItemProps> = ({
   onClick,
 }) => {
   return (
-    <div className="p-4 cursor-pointer">
-      <div className="flex items-center gap-5" onClick={() => onClick(teamId)}>
-        <div className="w-12 h-12 flex items-center justify-center bg-[#1E1E1E] text-white text-xl font-bold rounded-full">
-          {teamAvatar ? (
-            <img
-              src={teamAvatar}
-              alt={teamName}
-              className="w-full h-full rounded-full object-cover"
-            />
-          ) : (
-            teamName.charAt(0).toUpperCase()
-          )}
-        </div>
+    <div className="mx-2 my-6 cursor-pointer">
+      <div className="flex items-center gap-4" onClick={() => onClick(teamId)}>
+        {teamAvatar ? (
+          <Image
+            src={teamAvatar}
+            alt={teamName}
+            className="rounded-full object-cover"
+            width={36}
+            height={36}
+          />
+        ) : (
+          <div className="w-12 h-12 flex items-center justify-center bg-[#1E1E1E] text-white text-xl font-bold rounded-full">
+            {teamName.charAt(0).toUpperCase()}
+          </div>
+        )}
         <span className="text-lg font-medium">{teamName}</span>
       </div>
 
-      <div className="w-full h-[1px] bg-[#D6D6D6] mt-4 mx-auto" />
+      <div className="w-full h-[1px] bg-[#D6D6D6] mt-4" />
     </div>
   );
 };
