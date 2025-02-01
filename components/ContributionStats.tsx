c05b9cb046e0704d561629fd48d2469d72a50826
Aayush Shukla <oreus.aks@gmail.com>
2025-02-01 21:51:06 +0530

    misc: dummy contribution stats component

--- components/ContributionStats.tsx
+++ components/ContributionStats.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ContributionStats = () => {
-  return <div>ContributionStats</div>;
+  return (
+    <div className="w-full bg-white rounded-lg border border-[#D9D9D9] p-4">
+      <h1 className="pb-2 md:pb-4 text-lg md:text-2xl font-bold">
+        Contribution Stats
+      </h1>
+    </div>
+  );
 };
 
 export default ContributionStats;
