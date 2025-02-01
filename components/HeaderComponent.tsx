0a1e0edacf57e28fdd2520d37c4036f60409e06a
Aayush Shukla <oreus.aks@gmail.com>
2025-02-01 21:26:27 +0530

    fix: change header from fixed to absolute

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "./Button";
 const HeaderComponent = () => {
   return (
-    <header className="fixed top-0 left-0 z-50 flex justify-between items-center w-full bg-white py-3 px-6">
+    <header className="absolute top-0 left-0 z-50 flex justify-between items-center w-full bg-white py-3 px-6">
       <div className="text-lg">Logo</div>
       <div className="flex space-x-4">
         <Button
