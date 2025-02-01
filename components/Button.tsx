a305d8a3768499b610e13c3476b435acdb7d424c
Aayush Shukla <oreus.aks@gmail.com>
2025-02-01 21:20:13 +0530

    feat: button adapts to #39

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface ButtonProps {
   customBackgroundColor?: string;
   customTextColor?: string;
+  customBorderColor?: string;
   buttonText: string;
   onClick: () => void;
   customStyle?: string;
@@ -11,6 +12,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({
   customBackgroundColor = "#1E1E1E",
   customTextColor = "#F5F5F5",
+  customBorderColor,
   buttonText,
   onClick,
   customStyle = "",
@@ -21,6 +23,7 @@ const Button: React.FC<ButtonProps> = ({
       style={{
         backgroundColor: customBackgroundColor,
         color: customTextColor,
+        border: customBorderColor && `1px solid ${customBorderColor}`,
       }}
       className={`rounded-lg py-2 px-4 ${customStyle}`}
     >
