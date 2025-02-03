c042a001e011b5c1b8ec7cc1e251141e060514ca
Aayush Shukla <oreus.aks@gmail.com>
2025-02-03 21:06:17 +0530

    feat: add logic for login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,12 +3,53 @@ import React from "react";
 import Button from "@/components/Button";
 import InputField from "@/components/InputField";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import api from "@/api";
+import axios from "axios";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
 
-  const handleSubmit = () => {
-    console.log({ email, password });
+  const handleSubmit = async () => {
+    try {
+      const response = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        router.push("/");
+      }
+
+      setError("");
+      console.log(response.data);
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err)) {
+        if (err.response) {
+          switch (err.response.status) {
+            case 403:
+              setError("Invalid credentials. Please try again.");
+              break;
+            case 404:
+              setError("User not found. Please register.");
+              break;
+            case 500:
+              setError("Internal server error. Please try again later.");
+              break;
+            default:
+              setError("An unexpected error occurred.");
+          }
+        } else {
+          setError("Network error or server unreachable.");
+        }
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    }
   };
 
   return (
@@ -27,6 +68,7 @@ const Login = () => {
           onTextChange={(value) => setPassword(value)}
           customStyle="mt-4"
         />
+        {error && <p className="text-red-500 mt-4">{error}</p>}
         <Button
           buttonText="Sign In"
           onClick={handleSubmit}
