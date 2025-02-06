dd394f0331813dd4df2d410dbf329383e9fe91b5
Aayush Shukla <oreus.aks@gmail.com>
2025-02-06 21:31:08 +0530

    feat: join team page

--- app/joinTeam/page.tsx
+++ app/joinTeam/page.tsx
@@ -1,24 +1,36 @@
 "use client";
+
 import React, { useState } from "react";
 import InputField from "@/components/InputField";
 import Button from "@/components/Button";
 import DangerButton from "@/components/DangerButton";
+import { useRouter } from "next/navigation";
+import api from "@/api";
+import { useAuth } from "@/context/AuthContext";
 
 const JoinTeamPage: React.FC = () => {
   const [teamID, setTeamID] = useState("");
+  const router = useRouter();
+
+  const { user, loading, getUser } = useAuth();
+
+  const joinTeam = async () => {
+    const response = await api.post(`/team/${teamID}/join`);
+
+    if (response.status === 201) {
+      router.push("/teampage");
+    }
+  };
 
   const handleSubmit = () => {
-    console.log("Team ID:", teamID);
+    joinTeam();
   };
 
   return (
     <div className="bg-[#F3F4F6] w-full h-screen flex flex-col">
-      <header className="w-full bg-white flex items-center justify-between px-6 h-[10vh]">
+      <header className="w-full bg-white flex items-center justify-between px-6 py-3">
         <h1 className="text-lg font-medium">Join a Team</h1>
-        <DangerButton
-          buttonText="Cancel"
-          onClick={() => console.log("Cancelled")}
-        />
+        <DangerButton buttonText="Cancel" onClick={() => router.push("/")} />
       </header>
 
       <main className="flex justify-center items-center flex-1">
