fd937960e8b74ca7bc85ac9c67a57dc24e8965ba
Aayush Shukla <oreus.aks@gmail.com>
2025-02-06 21:34:31 +0530

    chore: put required buttons on header

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,22 +1,34 @@
 "use client";
+
 import React from "react";
 import Button from "./Button";
+import { useRouter } from "next/navigation";
+import DangerButton from "./DangerButton";
+
 const HeaderComponent = () => {
+  const router = useRouter();
+
   return (
     <header className="flex justify-between items-center w-full bg-white py-3 px-6 border-b border-b-[#D9D9D9]">
-      <div className="text-lg font-bold">Logo</div>
-      <div className="flex space-x-4">
+      <div
+        className="text-lg font-bold hover:cursor-pointer"
+        onClick={() => router.push("/")}
+      >
+        Logo
+      </div>
+      <div className="flex space-x-2">
         <Button
-          customBackgroundColor="#1E1E1E"
-          customTextColor="#F5F5F5"
-          buttonText="Button"
-          onClick={() => {}}
+          buttonText="Profile"
+          onClick={() => {
+            router.push("/user");
+          }}
         />
-        <Button
-          customBackgroundColor="#1E1E1E"
-          customTextColor="#F5F5F5"
-          buttonText="Button"
-          onClick={() => {}}
+        <DangerButton
+          buttonText="Logout"
+          onClick={() => {
+            localStorage.removeItem("token");
+            router.push("/login");
+          }}
         />
       </div>
     </header>
