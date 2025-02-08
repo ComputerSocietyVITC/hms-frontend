05edbd9116d51e930333d6e1ba89daa5fef07969
Aayush Shukla <oreus.aks@gmail.com>
2025-02-08 10:29:12 +0530

    feat: add github id and link page to edit profile

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "./Button";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 interface UserInformationProps {
   registrationNumber: string;
@@ -9,6 +11,7 @@ interface UserInformationProps {
   collegeName: string;
   phoneNumber: string;
   userId: string;
+  githubId: string | null;
   customStyle?: string;
 }
 
@@ -18,8 +21,18 @@ const UserInformation = ({
   collegeName,
   phoneNumber,
   userId,
+  githubId,
   customStyle,
 }: UserInformationProps) => {
+  const { user, getUser } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      getUser();
+    }
+  }, [user, getUser]);
+
   return (
     <div
       className={`w-full flex flex-col my-auto h-auto border border-[#D9D9D9] p-8 rounded-md bg-white justify-center ${customStyle}`}
@@ -34,22 +47,35 @@ const UserInformation = ({
           <p>{teamName}</p>
         </div>
       </div>
-      <div className="flex flex-col justify-between w-full mt-4">
-        <p className="font-bold">College Name</p>
-        <p>{collegeName}</p>
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col justify-between w-full mt-4">
+          <p className="font-bold">College Name</p>
+          <p>{collegeName}</p>
+        </div>
+        <div className="flex flex-col justify-between w-full mt-4">
+          <p className="font-bold">GitHub Profile URL</p>
+          <p>{githubId || "GitHub ID not set"}</p>
+        </div>
       </div>
       <div className="flex flex-col justify-between w-full mt-4">
         <p className="font-bold">Phone Number</p>
         <p>{phoneNumber}</p>
       </div>
-      <Button
+      {user!.id === userId && (
+        <Button
+          buttonText="Edit Profile"
+          customStyle="mt-6"
+          onClick={() => router.push("/editProfile")}
+        />
+      )}
+      {/* <Button
         onClick={() => alert(`Promoted user with ID: ${userId}`)}
         buttonText="Promote"
         customBackgroundColor="#14AE5C"
         customBorderColor="#02542D"
         customTextColor="#FFFFFF"
         customStyle="mt-6 w-fit"
-      />
+      /> */}
     </div>
   );
 };
