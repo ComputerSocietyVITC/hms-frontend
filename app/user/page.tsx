c5d1f58716e65798f6dece3cf2078781227c4f90
Aayush Shukla <oreus.aks@gmail.com>
2025-02-08 09:35:08 +0530

    feat: team name display

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
+import api from "@/api";
 import FooterSection from "@/components/FooterSection";
 import HeaderComponent from "@/components/HeaderComponent";
 import UserCard from "@/components/UserCard";
 import UserInformation from "@/components/UserInformation";
 import { useAuth } from "@/context/AuthContext";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Profile = () => {
   const { user, loading, getUser } = useAuth();
+  const [team, setTeam] = useState<string | null>(null);
+
+  const getTeam = async () => {
+    if (user && user.teamId) {
+      const response = await api.get(`/team/${user.teamId}`);
+      setTeam(response.data.name);
+    }
+  };
 
   useEffect(() => {
     if (!user) {
@@ -18,7 +27,7 @@ const Profile = () => {
   }, [user]);
 
   useEffect(() => {
-    console.log(user);
+    getTeam();
   }, [user]);
 
   if (loading) {
@@ -48,11 +57,11 @@ const Profile = () => {
           college={user.college}
           github={user.github || "https://github.com/notfound"}
           isLeader={user.isLeader}
-          customStyle="w-[25%]"
+          customStyle="w-[35%]"
         />
         <UserInformation
           registrationNumber={user.regNum}
-          teamName={user.teamId || "Not in a team"}
+          teamName={team || "Not in a team"}
           collegeName={user.college}
           phoneNumber={user.phone}
           userId={user.id}
