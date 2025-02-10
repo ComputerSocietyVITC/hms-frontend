cb8f3f85fd34c4e19ef167391a89c3b41626b9c0
Aayush Shukla <oreus.aks@gmail.com>
2025-02-11 01:09:57 +0530

    feat: dark mode and hover effects

--- components/TeamListItem.tsx
+++ components/TeamListItem.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 interface TeamListItemProps {
   teamId: string;
   teamName: string;
-  teamAvatar: string;
+  teamAvatar?: string;
   onClick: (teamId: string) => void;
 }
 
@@ -15,25 +15,29 @@ const TeamListItem: React.FC<TeamListItemProps> = ({
   onClick,
 }) => {
   return (
-    <div className="mx-2 my-6 cursor-pointer">
-      <div className="flex items-center gap-4" onClick={() => onClick(teamId)}>
+    <div>
+      <div
+        className="flex items-center gap-4 p-3 rounded-lg cursor-pointer transition-all duration-200 hover:bg-neutral-800"
+        onClick={() => onClick(teamId)}
+      >
         {teamAvatar ? (
           <Image
             src={teamAvatar}
             alt={teamName}
             className="rounded-full object-cover"
-            width={36}
-            height={36}
+            width={42}
+            height={42}
           />
         ) : (
-          <div className="w-12 h-12 flex items-center justify-center bg-[#1E1E1E] text-white text-xl font-bold rounded-full">
+          <div className="w-12 h-12 flex items-center justify-center bg-gray-700 text-white text-lg font-bold rounded-full">
             {teamName.charAt(0).toUpperCase()}
           </div>
         )}
-        <span className="text-lg font-medium">{teamName}</span>
+
+        <span className="text-lg font-medium text-gray-200">{teamName}</span>
       </div>
 
-      <div className="w-full h-[1px] bg-[#D6D6D6] mt-4" />
+      <div className="w-full h-[1px] bg-gray-600"></div>
     </div>
   );
 };
