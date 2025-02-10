b1e0dc0e64db94cf40870f4f30926949670786a9
Aayush Shukla <oreus.aks@gmail.com>
2025-02-10 22:57:22 +0530

    feat: homepage dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,15 @@ import FooterSection from "@/components/FooterSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col bg-[#F3F4F6]">
+    <main className="min-h-screen flex flex-col bg-[#09090b] text-white">
       <HeaderComponent />
 
       <div className="flex-1">
         <section className="flex items-center justify-center px-6 py-20">
-          <HeroSection title="HMS Title" description="HMS Subtitle" />
+          <HeroSection
+            title="Hackathon Management System"
+            description="IEEE Computer Society, VIT Chennai"
+          />
         </section>
 
         <section className="flex justify-center w-full px-8 mb-12">
