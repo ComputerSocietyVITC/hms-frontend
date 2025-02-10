fe016251078b85af1f18a334bc2bc27cc4cfbf95
Aayush Shukla <oreus.aks@gmail.com>
2025-02-10 22:51:53 +0530

    feat: footer dark mode

--- components/FooterSection.tsx
+++ components/FooterSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterSection = () => {
   return (
-    <footer className="text-center py-4 w-full border-t border-t-[#D9D9D9] bg-white font-bold">
+    <footer className="text-center py-4 w-full border-t border-gray-700 bg-[#121212] text-white font-bold">
       <p>Powered by IEEE Computer Society</p>
     </footer>
   );
