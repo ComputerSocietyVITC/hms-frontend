f92ea4090f68499b665e678e95ec88d9fa2824a1
Aayush Shukla <oreus.aks@gmail.com>
2025-02-10 22:56:34 +0530

    feat: card dark mode

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,18 +15,13 @@ const Card: React.FC<CardProps> = ({
   buttonOnClick,
 }) => {
   return (
-    <div className="bg-[#1E1E1E] text-white rounded-lg p-4 w-80 h-80 flex flex-col justify-between">
+    <div className="bg-[#121212] text-white rounded-lg p-6 w-80 h-80 flex flex-col justify-between shadow-lg border border-gray-700">
       <div className="flex flex-col">
         <h1 className="text-xl font-bold">{title}</h1>
-        <p className="text-sm">{description}</p>
+        <p className="text-sm text-gray-300 mt-2">{description}</p>
       </div>
       <div className="flex justify-end">
-        <Button
-          onClick={buttonOnClick}
-          buttonText={buttonText}
-          customBackgroundColor="#F5F5F5"
-          customTextColor="#2C2C2C"
-        />
+        <Button onClick={buttonOnClick} buttonText={buttonText} />
       </div>
     </div>
   );
