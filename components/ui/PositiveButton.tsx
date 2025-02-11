9ccd1c612691a835757f636e01407d04fc992f28
Aayush Shukla <oreus.aks@gmail.com>
2025-02-11 19:39:39 +0530

    feat: add important positive button

---
+++ components/ui/PositiveButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface ButtonProps {
+  buttonText: string;
+  onClick: () => void;
+  customStyle?: string;
+  disabled?: boolean;
+}
+
+const PositiveButton: React.FC<ButtonProps> = ({
+  buttonText,
+  onClick,
+  customStyle,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-lg py-2 px-4 bg-[#16A34A] hover:bg-[#15803D] border border-[#404040] text-white font-semibold disabled:opacity-50 disabled:cursor-not-allowed transition-all ${customStyle}`}
+      disabled={disabled}
+    >
+      {buttonText}
+    </button>
+  );
+};
+
+export default PositiveButton;
