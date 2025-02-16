b862d67dad15d009dc5e128a87dcd86d30047e70
Aayush Shukla <oreus.aks@gmail.com>
2025-02-16 15:15:37 +0530

    fix: remove unused buttons

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,27 +1,16 @@
 import React from "react";
-import Button from "./Button";
 
 interface CardProps {
   title: string;
   description: string;
-  buttonText: string;
-  buttonOnClick: () => void;
 }
 
-const Card: React.FC<CardProps> = ({
-  title,
-  description,
-  buttonText,
-  buttonOnClick,
-}) => {
+const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
     <div className="bg-[#121212] text-white rounded-lg p-4 w-80 h-80 flex flex-col justify-between shadow-lg border border-gray-700">
       <div className="flex flex-col">
         <h1 className="text-xl font-black">{title}</h1>
         <p className="text-sm text-gray-400">{description}</p>
-      </div>
-      <div className="flex justify-end">
-        <Button onClick={buttonOnClick} buttonText={buttonText} />
       </div>
     </div>
   );
